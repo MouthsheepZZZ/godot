9ea40cc321de0bfe1aff45927ce1a55a73592407
zhiyangwang <zhiyangwang@tecent.com>
2026-08-27 18:21:35 +0800

    Keep Local LRT injection probes spherical

--- scene/3d/local_lrt_volume_3d.cpp
+++ scene/3d/local_lrt_volume_3d.cpp
@@ -289,20 +289,6 @@ void LocalLRTVolume3D::_update_debug_probe_instances() {
 
 		Transform3D probe_transform = probe_scale_transform;
 		probe_transform.origin = get_probe_position(position);
-		if (debug_mode == DEBUG_MODE_INJECTION && !builder->get_probe(position).occupied) {
-			const Vector4 red = get_probe_injection(position, 0);
-			const Vector4 green = get_probe_injection(position, 1);
-			const Vector4 blue = get_probe_injection(position, 2);
-			Vector3 direction(
-					red.y * 0.2126 + green.y * 0.7152 + blue.y * 0.0722,
-					red.z * 0.2126 + green.z * 0.7152 + blue.z * 0.0722,
-					red.w * 0.2126 + green.w * 0.7152 + blue.w * 0.0722);
-			if (!direction.is_zero_approx()) {
-				direction.normalize();
-				probe_transform.basis = Basis(Quaternion(Vector3(0.0, 1.0, 0.0), direction)).scaled(Vector3(debug_probe_scale * 0.55, debug_probe_scale * 1.8, debug_probe_scale * 0.55));
-				probe_transform.origin += direction * debug_probe_scale * 0.8;
-			}
-		}
 		debug_probe_multimesh->set_instance_transform(index, probe_transform);
 		debug_probe_multimesh->set_instance_color(index, color);
 	}
